e6c9911b5f4eff5a5df955eaf79269e5ea763826
danifosy <mail@daniha.de>
2021-11-12 18:27:14 +0100

    Add routing from task list to add task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
+import { ButtonComponent } from './button/button.component';
+import { TaskCardComponent } from './task-card/task-card.component';
+import { TextInputComponent } from './text-input/text-input.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: TaskCardComponent },
+  {
+    path: 'add',
+    component: TextInputComponent,
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
