1687fe6cea2d02130aa499d6f4651eac66926203
danifosy <mail@daniha.de>
2021-11-14 16:07:31 +0100

    Refactor into TodoModule and add lazy loading

---
+++ src/app/todo/todo-routing.module.ts
@@ -0,0 +1,19 @@
+import { HomePageComponent } from './home-page/home-page.component';
+import { TextInputComponent } from './text-input/text-input.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  {
+    path: 'add',
+    component: TextInputComponent,
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TodoRoutingModule {}
